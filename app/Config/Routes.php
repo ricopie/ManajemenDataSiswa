a87f8473970129a3e49ed32c89d57626c6ae0299
ricopie <myiico.sorapps@hotmail.com>
2024-08-13 02:48:24 +0700

    Adding Closure in routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', static function () {
+    return view('welcome_message');
+});
 
 // Route Group: Admin
 $routes->group('admin', static function ($admin) {
