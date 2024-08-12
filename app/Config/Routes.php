17a0158224c31281bfb1b0d4261c46a5ec394e4d
ricopie <myiico.sorapps@hotmail.com>
2024-08-13 02:06:54 +0700

    Update admin route group

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Route Group: Admin
+$routes->group('admin', static function ($admin) {
+    $admin->get('/', 'Dashboard::index');
+});
