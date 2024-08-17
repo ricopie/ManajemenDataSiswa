73b4852db4c0a348a0abdd30e12a702b5a942bf7
Ricopie <myiico.sorapps@hotmail.com>
2024-08-17 21:31:37 +0700

    Update partials

--- app/Views/admin/master.php
+++ app/Views/admin/master.php
@@ -1,285 +1,45 @@
-<!DOCTYPE html>
+<?= $this->include('admin/_partial/__head') ?>
 
-<html lang="en">
+<!-- Navbar -->
+<?= $this->include('admin/_partial/__navbar') ?>
+<!--/Navbar -->
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= esc($pageName) ?> | MDS
-    </title>
+<!-- Sidebar -->
+<?= $this->include('admin/_partial/__sidebar') ?>
+<!--/Sidebar -->
 
-    <!-- <link rel="shortcut icon" href="" type="image/x-icon"> -->
-    <link rel="icon" type="image/png" size="128x128" href="<?= base_url('assets') ?>/img/AdminLTELogo.png">
+<!-- Content -->
+<div class="content-wrapper">
 
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?= base_url('assets') ?>/plugins/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="<?= base_url('assets') ?>/css/styles.min.css">
-
-</head>
-
-<body class="hold-transition sidebar-mini">
-    <div class="wrapper">
-
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="<?= base_url() ?>" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="https://adminlte.io/docs/3.1/" target="_blank" title="AdminLTE Documentation"
-                        class="nav-link">Docs</a>
-                </li>
-                <li class="nav-item d-none d-sm-inline-block">
-                    <a href="https://github.com/ricopie/ManajemenDataSiswa" target="_blank" title="Github Repo"
-                        class="nav-link">Github Repo</a>
-                </li>
-            </ul>
-
-            <ul class="navbar-nav ml-auto">
-
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                        <i class="fas fa-search"></i>
-                    </a>
-                    <div class="navbar-search-block">
-                        <form class="form-inline">
-                            <div class="input-group input-group-sm">
-                                <input class="form-control form-control-navbar" type="search" placeholder="Search"
-                                    aria-label="Search">
-                                <div class="input-group-append">
-                                    <button class="btn btn-navbar" type="submit">
-                                        <i class="fas fa-search"></i>
-                                    </button>
-                                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                        <i class="fas fa-times"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-comments"></i>
-                        <span class="badge badge-danger navbar-badge">3</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <a href="#" class="dropdown-item">
-
-                            <div class="media">
-                                <img src="<?= base_url('assets') ?>/img/avatar5.png" alt="User Avatar"
-                                    class="img-size-50 mr-3 img-circle">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        Brad Diesel
-                                        <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">Call me whenever you can...</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-
-                            <div class="media">
-                                <img src="<?= base_url('assets') ?>/img/avatar4.png" alt="User Avatar"
-                                    class="img-size-50 img-circle mr-3">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        John Pierce
-                                        <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">I got your message bro</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-
-                            <div class="media">
-                                <img src="<?= base_url('assets') ?>/img/avatar2.png" alt="User Avatar"
-                                    class="img-size-50 img-circle mr-3">
-                                <div class="media-body">
-                                    <h3 class="dropdown-item-title">
-                                        Nora Silvester
-                                        <span class="float-right text-sm text-warning"><i
-                                                class="fas fa-star"></i></span>
-                                    </h3>
-                                    <p class="text-sm">The subject goes here</p>
-                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                                </div>
-                            </div>
-
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-                    </div>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">15</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <span class="dropdown-header">15 Notifications</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-envelope mr-2"></i> 4 new messages
-                            <span class="float-right text-muted text-sm">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-users mr-2"></i> 8 friend requests
-                            <span class="float-right text-muted text-sm">12 hours</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-file mr-2"></i> 3 new reports
-                            <span class="float-right text-muted text-sm">2 days</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-                        <i class="fas fa-expand-arrows-alt"></i>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                        <i class="fas fa-th-large"></i>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-
-        <aside class="main-sidebar sidebar-light-indigo elevation-4">
-
-            <a href="<?= base_url() ?>" class="brand-link">
-                <img src="<?= base_url('assets') ?>/img/AdminLTELogo.png" alt="AdminLTE Logo"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">MDS</span>
-            </a>
-
-            <div class="sidebar">
-
-                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="<?= base_url('assets') ?>/img/<?= env('github.user.profile') ?>" class="img-circle elevation-2"
-                            alt="User Image">
-                    </div>
-                    <div class="info">
-                        <a href="<?= site_url('admin') ?>" class="d-block"><?= env('github.user.name') ?></a>
-                    </div>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0"><?= esc($pageName) ?></h1>
                 </div>
-
-                <div class="form-inline d-none invisible">
-                    <div class="input-group" data-widget="sidebar-search">
-                        <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                            aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-sidebar">
-                                <i class="fas fa-search fa-fw"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column nav-child-indent" data-widget="treeview"
-                        role="menu" data-accordion="false">
-
-                        <li class="nav-item">
-                            <a href="<?= site_url('admin') ?>" class="nav-link">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p>
-                                    Dashboard
-                                </p>
-                            </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="<?= site_url('admin/student') ?>" class="nav-link">
-                                <i class="nav-icon fas fa-user-graduate"></i>
-                                <p>
-                                    Student
-                                </p>
-                            </a>
-                        </li>
-
-                    </ul>
-                </nav>
-
-            </div>
-
-        </aside>
-
-        <div class="content-wrapper">
-
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0"><?= esc($pageName) ?></h1>
-                        </div>
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="#<?= base_url() ?>">Home</a></li>
-                                <li class="breadcrumb-item active"><?= esc($pageName) ?></li>
-                            </ol>
-                        </div>
-                    </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#<?= base_url() ?>">Home</a></li>
+                        <li class="breadcrumb-item active"><?= esc($pageName) ?></li>
+                    </ol>
                 </div>
             </div>
-
-            <div class="content">
-                <div class="container-fluid">
-
-                    <?= $this->renderSection('content'); ?>
-
-                </div>
-            </div>
-
         </div>
-
-        <aside class="control-sidebar control-sidebar-dark">
-
-            <div class="p-3">
-                <h5>Title</h5>
-                <p>Sidebar content</p>
-            </div>
-        </aside>
-
-        <footer class="main-footer text-sm">
-
-            <div class="float-right d-none d-sm-inline">
-                <span>Version 3.2.0 | PHP: <a href="<?= site_url('phpinfo') ?>" title="PHP Info" target="_blank"><?= PHP_VERSION ?></a> | Env: <?= ENVIRONMENT ?></span>
-            </div>
-
-            <strong>Copyright &copy; 2024 <a href="<?= base_url() ?>">MDS</a>.</strong> All rights reserved. <a href="https://codeigniter4.github.io/userguide/" title="User Guide CodeIgniter 4" target="_blank">User Guide</a>
-        </footer>
     </div>
 
-    <script src="<?= base_url('assets') ?>/plugins/jquery/jquery.min.js"></script>
-    <script src="<?= base_url('assets') ?>/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?= base_url('assets') ?>/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="<?= base_url('assets') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= base_url('assets') ?>/js/scripts.js"></script>
-    <script></script>
-</body>
+    <div class="content">
+        <div class="container-fluid">
 
-</html>
+            <?= $this->renderSection('content'); ?>
+
+        </div>
+    </div>
+
+</div>
+<!--/Content -->
+
+<!-- Footer -->
+<?= $this->include('admin/_partial/__foot_copyright') ?>
+<!--/Footer -->
+
+<?= $this->include('admin/_partial/__footer') ?>
