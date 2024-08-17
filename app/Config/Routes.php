8844d232aa6d7b88c9d0cf4e5a4f85e9b27a22c5
Ricopie <myiico.sorapps@hotmail.com>
2024-08-17 20:12:08 +0700

    Adding route environment

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,7 +8,9 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', static function () {
     return view('welcome_message');
 });
-
+$routes->environment('development', static function ($env) {
+    $env->get('phpinfo', 'Home::phpInfo');
+});
 // Route Group: Admin
 $routes->group('admin', static function ($admin) {
     $admin->get('/', 'Dashboard::index', ['as' => 'dashboard']);
