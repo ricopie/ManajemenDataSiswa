b6c1c54f363ae7d639c1cf76199e8320b902dfd4
Ricopie <myiico.sorapps@hotmail.com>
2024-08-17 21:32:19 +0700

    Update script

--- app/Views/pages/student.php
+++ app/Views/pages/student.php
@@ -1,6 +1,5 @@
 <?= $this->extend('admin/master') ?>
 <?= $this->section('content'); ?>
-
 <div class="row">
     <div class="col-12">
         <div class="card">
@@ -8,31 +7,19 @@
                 <a href="#" class="btn bg-gradient-primary">Tambah Data</a>
             </div>
             <div class="card-body">
-                <table class="table table-hover table-bordered" id="tbl_dataStudent">
+                <table class="table table-hover table-bordered data-example">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th>Nomor Induk</th>
-                            <th>Foto</th>
+                            <th class="no-sort">Foto</th>
                             <th>Nama Lengkap</th>
-                            <th>&nbsp;</th>
+                            <th class="no-sort">&nbsp;</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <!--<tr>
-                            <th scope="row">1</th>
-                            <td>123456</td>
-                            <td>0</td>
-                            <td>Fulan bin Fulan</td>
-                            <td class="text-center">
-                                <div class="btn-group">
-                                    <a href="#" class="btn btn-sm btn-primary"><i class="fa fa-eye"></i></a>
-                                    <a href="#" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></a>
-                                </div>
-                            </td>
-                            <?= base_url('assets') ?>/img/<?= env('github.user.profile') ?>
-                        </tr>-->
                         <?php foreach ($faker as $i => $fake) : ?>
+
                             <tr>
                                 <td><?= $i + 1 ?></td>
                                 <td><?= $fake['number'] ?></td>
@@ -46,13 +33,22 @@
                                 </td>
                             </tr>
                         <?php endforeach; ?>
+
                     </tbody>
                 </table>
-
-
             </div>
         </div>
     </div>
 </div>
-
+<?= $this->section('script'); ?>
+<script {csp-script-nonce}>
+    $(".data-example").DataTable({
+        "button": ["print", "csv"],
+        "columnDefs": [{
+            "sortable": false,
+            "targets": "no-sort"
+        }]
+    }).button();
+</script>
+<?= $this->endSection(); ?>
 <?= $this->endSection(); ?>
